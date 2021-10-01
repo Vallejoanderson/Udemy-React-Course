17795ec1081155c14306de88a407d6a206bf2083
Anderson Vallejo <vallejoanderson@gmail.com>
2021-10-01 07:32:08 -0500

    Class

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,8 @@
-console.log('Hi');
+const personajes = ['Goku', 'Vegeta', 'Trunks'];
+
+const [ , , p3] = personajes;
+console.log( p3 );
+
+const retornaArreglo =  () => {
+    return ['ABC', 123];
+}
