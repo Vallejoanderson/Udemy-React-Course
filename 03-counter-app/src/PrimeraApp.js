fff5a1bd724bd1faf9036826237f4632d96f48d6
Anderson Vallejo <vallejoanderson@gmail.com>
2021-10-03 22:07:51 -0500

    Props

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+// rc
+
+const PrimeraApp = ( { saludo, subtitulo } ) => {
+
+    // if ( ! saludo ){
+    //     throw new Error('El saludo es necesario');
+    // }
+
+    console.log( saludo );
+
+    return (
+        <>
+            <h1> { saludo }</h1>
+            {/* <pre>{ JSON.stringify( saludo, null, 3 ) }</pre> */}
+            <p>{ subtitulo }</p>
+        </>
+    );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired,
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo',
+}
+
+export default PrimeraApp;
