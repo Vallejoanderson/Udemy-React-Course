60b6b77c13aa98d6cc08f840f499eb710c4cac09
vallejoanderson <vallejoanderson@gmail.com>
2021-10-08 17:39:25 -0700

    Tests

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -4,8 +4,14 @@ import CounterApp from "../CounterApp";
 import { shallow } from 'enzyme'
 
 describe('Pruebas en <CounterApp />', () => {
+
+    let wrapper = shallow(<CounterApp />);
+
+    beforeEach( () => {
+        wrapper = shallow(<CounterApp />);
+    });
+    
     test('Debe de mostrar un snapshot con los valores por defecto', () => {
-        const wrapper = shallow(<CounterApp />);
 
         expect(wrapper).toMatchSnapshot();
     })
@@ -16,6 +22,37 @@ describe('Pruebas en <CounterApp />', () => {
 
         const valueH2 = wrapper.find('h2').text();
         expect( parseInt(valueH2) ).toBe( valor );
+    })
+
+
+    test('Deberia sumar + 1 al hacer click en el button +1', () => {
+
+        wrapper.find('button').at(0).simulate('click'); 
+        const counterText = wrapper.find('h2').text().trim();
+    
+        expect( counterText ).toBe('11');
+
+    })
+
+    test('Deberia restar -1 al hacer click en el button -1', () => {
+
+        wrapper.find('button').at(2).simulate('click'); 
+        const counterText = wrapper.find('h2').text().trim();
+    
+        expect( counterText ).toBe('9');
+
+    })
+
+    test('Deberia resetear el valor por defecto en el componente', () => {
+        
+        const wrapper = shallow( <CounterApp value={ 105 }/> );
+        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(0).simulate('click');
+
+        const counterText = wrapper.find('h2').text().trim;
+
+        expect( counterText) .toBe('105')
+
     })
     
 })
