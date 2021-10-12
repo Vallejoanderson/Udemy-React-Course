7951c2cf0a0b2a0360e0e7ca17a2973889445c1b
vallejoanderson <vallejoanderson@gmail.com>
2021-10-11 18:59:49 -0700

    Custom Hook

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs'
-import { GifGridItem } from './GifGridItem'
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+// import { GifGridItem } from './GifGridItem'
+// import { getGifs } from '../helpers/getGifs'
 
 export const GifGrid = ( { category } ) => {
 
-    const [images, setImages] = useState([])
+    // const [images, setImages] = useState([]);
     
-    useEffect( () => {
-        getGifs( category )
-            .then( imgs => setImages( imgs) ) 
-    }, [ category ] )
+    const { loading } = useFetchGifs();
+    console.log( loading );
+
+    // useEffect( () => {
+    //     getGifs( category )
+    //         .then( imgs => setImages( imgs) ) 
+    // }, [ category ] )
 
     
     //getGifs();
@@ -17,7 +21,8 @@ export const GifGrid = ( { category } ) => {
     return (
         <>
             <h3>{  category }</h3>
-            <div className="card-grid">
+            { loading ? 'Cargando...' : 'Data cargada'}
+            {/* <div className="card-grid">
                 {
                         images.map( img => //Desestructuración de objecto images
                             <GifGridItem 
@@ -26,7 +31,7 @@ export const GifGrid = ( { category } ) => {
                             />
                         )
                 }
-            </div>
+            </div> */}
         </>
     )
 }
