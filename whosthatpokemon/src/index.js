85d36ddb051cfee60ecefd2338f2746b4fe686c2
vallejoanderson <vallejoanderson@gmail.com>
2021-10-25 18:14:12 -0700

    Doing merge

--- whosthatpokemon/src/index.js
+++ whosthatpokemon/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 export const getPokemons = async() => {
 
   const url = `https://pokeapi.co/api/v2/pokemon`;
+  
   const resp = await fetch(url);
 
   const data  = await resp.json();
