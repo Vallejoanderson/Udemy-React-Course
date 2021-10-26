706d864da9aa27508174c3faa1a6afc629b0b3ee
vallejoanderson <vallejoanderson@gmail.com>
2021-10-25 18:48:56 -0700

    API conectada

--- whosthatpokemon/src/WhosThatPokemonApp.js
+++ whosthatpokemon/src/WhosThatPokemonApp.js
@@ -1,12 +1,19 @@
 import getPokemon from './helpers/getPokemon'
+import { useState } from 'react'
 
 const WhosThatPokemonApp = () => {
+
+  const [ image, setImage ] = useState('');
+
   getPokemon()
-    .then( img => { pokemon = img })
+    .then( img => 
+          { setImage(img) }
+    )
+
   return(
     <div>
         <p>Hola</p>
-        <img  src={ pokemon } alt="Random pokemon"/>
+        <img  src={ image } alt="Random pokemon"/>
     </div>
   )
 }
--- whosthatpokemon/src/index.js
+++ whosthatpokemon/src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
-
-export const getPokemons = async() => {
-
-  const url = `https://pokeapi.co/api/v2/pokemon`;
-  
-  const resp = await fetch(url);
-
-  const data  = await resp.json();
-
-  console.log( data );
-}
-
-getPokemons();
-=======
-import WhosThatPokemonApp from './WhosThatPokemonApp.js'
->>>>>>> c083308daf91e4115407bc913264060eac8fd27c
+import WhosThatPokemonApp from './WhosThatPokemonApp'
 
 ReactDOM.render(
   <WhosThatPokemonApp />,
